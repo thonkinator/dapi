7d975ed61dea8558338c154daa7adfbbae6da396
Patrick Martin <me@psm.pm>
2023-09-02 16:57:20 -0400

    trying caching again still not working in dev but idk

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,19 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>();
 app.use("*", async (c, next) => {
 	const start = performance.now();
+	const cached = await caches.default.match(c.req.url);
+	const bypass = c.req.headers.get("Cache-Control")?.includes("no-cache");
+	if (cached && !bypass) {
+		console.log(cached);
+		const res = new Response(cached.body);
+		res.headers.set("X-Cache-Status", "HIT");
+		return res;
+	}
 	await next();
 	c.res.headers.set("X-Response-Time", `${performance.now() - start}`);
-	if (new URL(c.req.url).searchParams.has("reload")) {
-		c.res.headers.set("Cache-Control", "no-store");
-	} else {
-		c.res.headers.set("Cache-Control", "public, max-age=3600");
-	}
+	if (!c.res.ok) return;
+	c.res.headers.set("X-Cache-Status", bypass ? "BYPASS" : "MISS");
+	c.executionCtx.waitUntil(caches.default.put(c.req.url, c.res.clone()));
 });
 
 async function api(c: Context<{ Bindings: Bindings }>, endpoint: string) {
@@ -31,7 +37,11 @@ async function cdn(c: Context<{ Bindings: Bindings }>, endpoint: string) {
 	const url = new URL(`https://cdn.discordapp.com${endpoint}`);
 	new URL(c.req.url).searchParams.forEach((value, param) => url.searchParams.append(param, value));
 	const res = await fetch(url);
-	return new Response(res.body, res);
+	return new Response(res.body, {
+		headers: {
+			"Content-Type": res.headers.get("Content-Type")!,
+		},
+	});
 }
 
 app.get(
